eb2bce2c67e7e32d3e26a7be39f3ff059e8672e2
Syssos <paral.cody98@gmail.com>
2018-09-27 11:36:59 -0700

    added new app

--- app.py
+++ app.py
@@ -1,46 +1,78 @@
-from flask import Flask, make_response, jsonify, Response, abort, render_template, redirect
+"""
+    GitHub Example
+    --------------
+    Shows how to authorize users with Github.
+"""
+from flask import Flask, request, g, session, redirect, url_for
+from flask import render_template_string
 from flask_github import GitHub
-import os
-from flask_cors import CORS
-from dbs import storage
-from loginmodel import UserCred
 
-# I added CORS just in case -- may not need
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+DATABASE_URI = 'sqlite:////tmp/github-flask.db'
+SECRET_KEY = 'development key'
+DEBUG = True
+
+# Set these values
+GITHUB_CLIENT_ID = 'c289d6463aee4a1f4fef'
+GITHUB_CLIENT_SECRET = '989605368ed211c6f7472372c7c9111895d45eca'
+
+# setup flask
 app = Flask(__name__)
-app.config['GITHUB_CLIENT_ID'] = 'c289d6463aee4a1f4fef'
-app.config['GITHUB_CLIENT_SECRET'] = '989605368ed211c6f7472372c7c9111895d45eca'
+app.config.from_object(__name__)
 
-# Pretty Print
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
-# For GitHub Enterprise
-app.config['GITHUB_BASE_URL'] = 'https://HOSTNAME/api/v3/'
-app.config['GITHUB_AUTH_URL'] = 'https://HOSTNAME/login/oauth/'
-
+# setup github-flask
 github = GitHub(app)
 
-@app.route('/login')
-def login():
-    return github.authorize()
+# setup sqlalchemy
+engine = create_engine(app.config['DATABASE_URI'])
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
 
-@app.route('/github-callback')
-@github.authorized_handler
-def authorized(oauth_token):
-    next_url = request.args.get('next') or url_for('index')
-    if oauth_token is None:
-        flash("Authorization failed.")
-        return redirect(next_url)
 
-    user = User.query.filter_by(github_access_token=oauth_token).first()
-    if user is None:
-        user = User(oauth_token)
-        db_session.add(user)
+def init_db():
+    Base.metadata.create_all(bind=engine)
 
-    user.github_access_token = oauth_token
-    db_session.commit()
-    return redirect(next_url)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(200))
+    github_access_token = Column(String(200))
+
+    def __init__(self, github_access_token):
+        self.github_access_token = github_access_token
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = User.query.get(session['user_id'])
+
+
+@app.after_request
+def after_request(response):
+    db_session.remove()
+    return response
+
+
+@app.route('/')
+def index():
+    if g.user:
+        t = 'Hello! <a href="{{ url_for("user") }}">Get user</a> ' \
+            '<a href="{{ url_for("logout") }}">Logout</a>'
+    else:
+        t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
+
+    return render_template_string(t)
+
 
 @github.access_token_getter
 def token_getter():
@@ -48,7 +80,53 @@ def token_getter():
     if user is not None:
         return user.github_access_token
 
+
+@app.route('/github-callback')
+@github.authorized_handler
+def authorized(access_token):
+    next_url = request.args.get('next') or url_for('index')
+    if access_token is None:
+        return redirect(next_url)
+
+    user = User.query.filter_by(github_access_token=access_token).first()
+    if user is None:
+        user = User(access_token)
+        db_session.add(user)
+    user.github_access_token = access_token
+    db_session.commit()
+
+    session['user_id'] = user.id
+    return redirect(next_url)
+
+
+@app.route('/login')
+def login():
+    if session.get('user_id', None) is None:
+        return github.authorize()
+    else:
+        return 'Already logged in'
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/user')
+def user():
+    a = github.get('user')
+    return str(a['name'])
+
+
 @app.route('/repo')
-def repo():
-    repo_dict = github.get('repos/cenkalti/github-flask')
-    return str(repo_dict)
+def repos_list():
+    a = github.get('user')
+    url = a['repos_url']
+    b = github.get('Syssos/repos')
+    return str(b)
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
